01f936dd42042607a31dd0c52522dcc273b9095a
HuyenTranz <tranthihuyentran982003@gmail.com>
2025-11-26 15:11:58 +0700

    #4 feat(project): complete UI projectPage and fix ui menu

--- fe-moodyplan/src/pages/project/ProjectPage.jsx
+++ fe-moodyplan/src/pages/project/ProjectPage.jsx
@@ -1,27 +1,79 @@
 import { useParams } from 'react-router-dom'
 import Header from '../../components/dashboard/Header'
+import { useState } from 'react';
+import ModalAddSection from '../../components/modal/ModalAddSection';
+import { RiAddLine } from "react-icons/ri";
+import ModalAddTask from '../../components/modal/ModalAddTask';
 
 const ProjectPage = () => {
-    const { project_id, title } = useParams();
+    const { project_id, project_title } = useParams();
+    const [empty, setEmpty] = useState(true);
+    const [isOpenModalAddTask, setOpenModalAddTask] = useState(false);
+    const [isOpenModalSection, setOpenModalSection] = useState(false);
 
+
+    const handleOpenModal = () => setOpenModalAddTask(true);
+    const handleCloseModal = () => setOpenModalAddTask(false);
+
+    const handleOpenModalAddTask = () => {
+        setOpenModalAddTask(true);
+        setEmpty(false);
+    }
+
+    const handleCloseModalAddTask = () => {
+        setOpenModalAddTask(false);
+        setEmpty(true);
+    };
+
+    const handleOpenModalSection = () => {
+        setOpenModalSection(true);
+        setEmpty(false);
+    }
+
+    const handleCloseModalSection = () => {
+        setOpenModalSection(false);
+        // setEmpty(true);
+    };
     return (
-        <div className="project-page">
-            <Header 
-                displayTitle={title}
-                isProjectPage={true}
-            />
+        <div className="project-page-wapper">
+            <Header />
 
-            <div className="project-content" style={{
-                backgroundColor: '#f4f4f4',
-                borderRadius: '8px',
-                padding: '20px',
-                height: "200vh"
-            }}>
-                <p>This is the project page for project:</p>
+            <div className="project-content" >
+                {/* Hiển thị tiêu đề động */}
+                <div className="big-page-title">{project_title}</div>
+
+                {/* Add task */}
+                <div className="task-lists">
+                    <div className="add-task">
+                        <button className="add-task-btn" onClick={handleOpenModal}>
+                            <RiAddLine /> Add task
+                        </button>
+                    </div>
+                    {/* Chỉ hiện Add section khi modal chưa mở */}
+                    {!isOpenModalSection && (
+                        <div className="divider-wrapper">
+                            <span className="line"></span>
+                            <button className="btn-text" onClick={handleOpenModalSection}>
+                                Add section
+                            </button>
+                            <span className="line"></span>
+                        </div>
+                    )}
+
+                    {/* Modal Section */}
+                    {isOpenModalSection && (
+                        <div className="modal-backdrop-inbox">
+                            <ModalAddSection onClose={handleCloseModalSection} />
+                        </div>
+                    )}
+                </div>
             </div>
-
-            <h1>{title}</h1>
-            <p>Project ID: {project_id}</p>
+            {/* Modal add task */}
+            {isOpenModalAddTask && (
+                <div className="modal-backdrop">
+                    <ModalAddTask onClose={handleCloseModal} />
+                </div>
+            )}
         </div>
     )
 }
